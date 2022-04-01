5c4cdfdf921de742c6764f120fa6588b961dfc3d
Daniel Alves <danielribeiroalves0@gmail.com>
2022-03-31 22:18:55 -0300

    Arrumando changeset

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -1,10 +1,11 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
-  alias Discuss.Topics
   alias Discuss.Topics.Topic
 
   def new(conn, params) do
-    changeset = Discuss.Topics.Topic.changeset(%Discuss.Topics.Topic{}, %{})
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render(conn, "new.html")
   end
 end
