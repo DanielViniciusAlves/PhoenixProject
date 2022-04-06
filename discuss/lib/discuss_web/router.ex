cc78b91dfe24299787f42cfb4f05b9672e9ad46b
Daniel Alves <danielribeiroalves0@gmail.com>
2022-04-05 22:52:46 -0300

    OAuth autenticacao

--- discuss/lib/discuss_web/router.ex
+++ discuss/lib/discuss_web/router.ex
@@ -17,12 +17,14 @@ defmodule DiscussWeb.Router do
   scope "/", DiscussWeb do
     pipe_through(:browser)
 
-    # get("/", TopicController, :index)
-    # post("/topics", TopicController, :create)
-    # get("/topics/new", TopicController, :new)
-    # get("/topics/:id/edit", TopicController, :edit)
-    # put("/topics/:id", TopicController, :update)
-    resources("/topics", TopicController)
+    resources("/", TopicController)
+  end
+
+  scope "/auth", DiscussWeb do
+    pipe_through(:browser)
+
+    get("/:provider", AuthController, :request)
+    get("/:provider/callback", AuthController, :callback)
   end
 
   # Other scopes may use custom stacks.
